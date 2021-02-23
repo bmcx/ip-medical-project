2e8e424425b53c420dabd596e7b2d2314b54a029
psychonelli <60044237+psychonelli@users.noreply.github.com>
2021-02-23 12:03:38 +0530

    updated history and diagnosis
    have to connect

--- src/pages/Doctor/Home/Components/Diagnosis.js
+++ src/pages/Doctor/Home/Components/Diagnosis.js
@@ -1,3 +1,25 @@
 export const Diagnosis = () => {
-  return <div>Diagnosis</div>;
+  return (
+    <div>
+          <div className="flex bg-gray-100 rounded-x1 p-8">
+          
+            <div className="flex-1  text-center  px-4 py-2 m-2
+             bg-blue-500  grid grid-flow-col grid-rows-2 grid-cols-3 gap-4 border 
+             rounded-lg border-blue-400 hover:bg-blue-600">Syntopms
+             </div>
+            <div className="flex-1  text-center  px-4 py-2 m-2
+             bg-blue-500  grid grid-flow-col grid-rows-2 grid-cols-3 gap-4 border 
+             rounded-lg border-blue-400 hover:bg-blue-600">threatment
+             </div>
+             <div className="flex-1  text-center  px-4 py-2 m-2
+            bg-blue-500  grid grid-flow-col grid-rows-2 grid-cols-3 gap-4 border 
+             rounded-lg border-blue-400 hover:bg-blue-600">Referral</div>
+        
+          
+                  
+        </div>
+     
+
+    </div>
+  )
 };
--- src/pages/Doctor/Home/Components/History.js
+++ src/pages/Doctor/Home/Components/History.js
@@ -1,36 +1,48 @@
 export const History = () => {
   return (
-    <div>
-      <section>
-        <figure className="bg-gray-100 rounded-x1 p-8">
-          <img
-            className="w-32 h-32 rounded-full max-auto self-center"
-            src="C:\Users\Dilshan\Downloads"
-          ></img>
-          <div className=" pt-4   space-y-4">
-            <figcaption className="font-medium">
-              <div>name</div>
-              <div>age</div>
-              <div>medical History</div>
-            </figcaption>
-          </div>
-        </figure>
-      </section>
-      <section>
-        <figure className="bg-gray-100 rounded-x1 p-8">
-          <img
-            className="w-32 h-32 rounded-full max-auto self-center"
-            src="C:\Users\Dilshan\Downloads"
-          ></img>
-          <div className=" pt-4  space-y-4">
-            <figcaption className="font-medium">
-              <div>name</div>
-              <div>age</div>
-              <div>medical History</div>
-            </figcaption>
-          </div>
-        </figure>
-      </section>
-    </div>
+    <div className="flex bg-gray-100 rounded-x1 p-8">
+       <div className="m-5 mt-4 ml-10 sm:flex ">
+         <a href="" className="flex item-center no-underline">
+           <div className="flex-shrink-0">
+             <img className="h-14 w-14 rounded-full border-2 border-white" src="logo192.png"></img>
+             <div className="">Name</div>
+             <div>Age</div>
+             <div>Medical Description </div>
+           </div>
+         </a>
+       </div> 
+       <div className="m-5 mt-4 ml-20 sm:flex">
+         <a href="" className="flex item-center no-underline">
+           <div className="flex-shrink-0">
+             <img className="h-14 w-14 rounded-full border-2 border-white" src="logo192.png"></img>
+             <div className="">Name</div>
+             <div>Age</div>
+             <div>Medical Description </div>
+           </div>
+         </a>
+         </div>
+         <div className="m-5 mt-4 ml-20 sm:flex">
+         <a href="" className="flex item-center no-underline">
+           <div className="flex-shrink-0">
+             <img className="h-14 w-14 rounded-full border-2 border-white" src="logo192.png"></img>
+             <div className="">Name</div>
+             <div>Age</div>
+             <div>Medical Description </div>
+           </div>
+         </a>
+         </div>
+       
+       
+
+
+
+
+
+
+
+
+
+       
+      </div>
   );
 };
