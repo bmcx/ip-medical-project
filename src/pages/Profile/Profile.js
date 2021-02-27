a468f58429f9230c8f1e39314ecca5784c5c9324
psychonelli <60044237+psychonelli@users.noreply.github.com>
2021-02-27 23:47:00 +0530

    update edit profile

--- src/pages/Profile/Profile.js
+++ src/pages/Profile/Profile.js
@@ -1,4 +1,92 @@
+import FormInput from "../../common/components/FormInput"
+
 const Profile = () => {
-  return <div>Profile</div>;
+  return (
+    <div className="ml-10 mt-5"> Edit Profile
+      <div className="flex rounded-x1 p-8 ml-20">
+        <div className="m-5 mt-4 ml-10 sm:flex ">
+          <a href="" className="flex item-center no-underline">
+            <div className="flex-shrink-0">
+              <img className="h-28 w-28 rounded-full border-2 border-white" src="logo192.png"></img>
+            </div>
+          </a>
+        </div>
+
+
+        <div className="m-10 ml-40">
+          <div className="flex justify-between items-center">
+
+            <FormInput placeholder="First Name" className="w-full mx-w-lg">
+              <div className="flex flex-wrap -mx-3 mb6"></div>
+              <input className="flex"> </input>
+
+
+            </FormInput>
+            <FormInput placeholder="Surname" className="w-full mx-w-lg ">
+              <div className="flex flex-wrap -mx-3 mb6"></div>
+              <input className="flex"> </input>
+            </FormInput>
+
+          </div>
+          <div>
+            <FormInput placeholder="Adreess" className=" mx-w-lg">
+              <div className="flex flex-wrap -mx-3 mb6"></div>
+              <input className="flex"> </input>
+
+            </FormInput>
+            <div> <FormInput placeholder="E-mail" className="w-full mx-w-lg">
+              <div className="flex flex-wrap -mx-3 mb6"></div>
+              <input className="flex"> </input>
+
+
+            </FormInput>
+               <FormInput placeholder="Mobile Number" className="w-full mx-w-lg">
+                <div className="flex flex-wrap -mx-3 mb6"></div>
+                <input className="flex"> </input>
+
+
+              </FormInput>
+
+                <FormInput placeholder="Occupation" required="ture" className="w-full mx-w-lg">
+                  <div className="flex flex-wrap -mx-3 mb6"></div>
+                  <input className="flex"> </input>
+
+
+                </FormInput>
+                <FormInput placeholder="Gender" required="ture" className="w-5">
+                  <div className="flex flex-wrap -mx-3 mb6"></div>
+                  <input className="flex"> </input>
+
+
+
+                </FormInput>
+                <FormInput placeholder="Date of Birth" required="ture" className="w-5">
+                  <div className="flex flex-wrap -mx-3 mb6"></div>
+                  <input className="flex"> </input>
+                </FormInput>
+                <FormInput placeholder="Height and Weight" required="ture" className="w-5">
+                  <div className="flex flex-wrap -mx-3 mb6"></div>
+                  <input className="flex"> </input>
+                </FormInput>
+
+
+
+
+              </div>
+            </div>
+
+
+          </div>
+        </div>
+      </div>
+      
+
+
+
+
+
+  )
 };
+
+
 export default Profile;
