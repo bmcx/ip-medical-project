3422c062e67707c82a97a83b207ac149b24c4507
psychonelli <60044237+psychonelli@users.noreply.github.com>
2021-03-01 14:54:03 +0530

    Update edit profile

--- src/pages/Profile/Profile.js
+++ src/pages/Profile/Profile.js
@@ -17,69 +17,91 @@ const Profile = () => {
           <div className="flex justify-between items-center">
 
             <FormInput placeholder="First Name" className="w-full mx-w-lg">
-              <div className="flex flex-wrap -mx-3 mb6"></div>
+              <div className="flex flex-wrap mx-3 mb6"></div>
               <input className="flex"> </input>
 
 
             </FormInput>
             <FormInput placeholder="Surname" className="w-full mx-w-lg ">
-              <div className="flex flex-wrap -mx-3 mb6"></div>
+              <div className="flex flex-wrap mx-3 mb6"></div>
               <input className="flex"> </input>
             </FormInput>
 
           </div>
           <div>
             <FormInput placeholder="Adreess" className=" mx-w-lg">
-              <div className="flex flex-wrap -mx-3 mb6"></div>
+              <div className="flex flex-wrap mx-3 mb6"></div>
               <input className="flex"> </input>
 
             </FormInput>
             <div> <FormInput placeholder="E-mail" className="w-full mx-w-lg">
-              <div className="flex flex-wrap -mx-3 mb6"></div>
+              <div className="flex flex-wrap mx-3 mb6"></div>
               <input className="flex"> </input>
 
 
             </FormInput>
-               <FormInput placeholder="Mobile Number" className="w-full mx-w-lg">
-                <div className="flex flex-wrap -mx-3 mb6"></div>
+              <FormInput placeholder="Contact Number" className="w-full mx-w-lg">
+                <div className="flex flex-wrap mx-3 mb6"></div>
                 <input className="flex"> </input>
 
 
               </FormInput>
 
-                <FormInput placeholder="Occupation" required="ture" className="w-full mx-w-lg">
-                  <div className="flex flex-wrap -mx-3 mb6"></div>
+              <FormInput placeholder="Occupation" required="ture" className="w-full mx-w-lg">
+                <div className="flex flex-wrap mx-3 mb6"></div>
+                <input className="flex"> </input>
+
+
+              </FormInput>
+              <FormInput placeholder="Gender" required="ture" className="w-5">
+                <div className="flex flex-wrap mx-3 mb6"></div>
+                <input className="flex"> </input>
+
+
+
+              </FormInput>
+              <div className="mt-2">
+                <div className="flex justify-between items-center">
+
+                  <FormInput placeholder="Day" required="ture" className="w-5">
+                  <div className="flex flex-wrap mx-3 mb6 "></div>
                   <input className="flex"> </input>
+                  </FormInput>
 
-
-                </FormInput>
-                <FormInput placeholder="Gender" required="ture" className="w-5">
-                  <div className="flex flex-wrap -mx-3 mb6"></div>
-                  <input className="flex"> </input>
-
-
-
-                </FormInput>
-                <FormInput placeholder="Date of Birth" required="ture" className="w-5">
-                  <div className="flex flex-wrap -mx-3 mb6"></div>
+                <FormInput placeholder="Month" required="ture" className="w-5">
+                  <div className="flex flex-wrap mx-3 mb6"></div>
                   <input className="flex"> </input>
                 </FormInput>
-                <FormInput placeholder="Height and Weight" required="ture" className="w-5">
-                  <div className="flex flex-wrap -mx-3 mb6"></div>
+                <FormInput placeholder="Year" required="ture" className="w-5">
+                  <div className="flex flex-wrap mx-3 mb6"></div>
                   <input className="flex"> </input>
                 </FormInput>
-
-
-
-
+              </div>
+            </div>
+            <div className="mt-2 mr-96">
+              <div className="flex justify-between items-center">
+                <FormInput placeholder="Height" required="ture" className="w-5">
+                  <div className="flex flex-wrap mx-3 mb6"></div>
+                  <input className="flex"> </input>
+                </FormInput>
+                <FormInput placeholder="Weight" required="ture" className="w-5">
+                  <div className="flex flex-wrap mx-3 mb6"></div>
+                  <input className="flex"> </input>
+                </FormInput>
               </div>
             </div>
 
 
+
+
           </div>
         </div>
+
+
       </div>
-      
+    </div>
+    </div >
+
 
 
 
