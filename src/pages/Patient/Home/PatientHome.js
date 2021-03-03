0469e47c102d8fbfc497eebf876cde8ac9269e14
JanithKE <49825461+JanithKE@users.noreply.github.com>
2021-03-03 20:27:47 +0530

    init

--- src/pages/Patient/Home/PatientHome.js
+++ src/pages/Patient/Home/PatientHome.js
@@ -1,7 +1,48 @@
 const PatientHome = () => {
     return (
         <div>
-            patient home
+            <div className="bg-gray-100 rounded-lg shadow">
+                <div className="p-9 font-mono">
+                    <h1 className="text-4xl">User name</h1>
+                    <h3 className="text-xl">User id</h3>
+                </div>
+                <div>
+                    
+                </div>
+            </div>
+            <div>
+                <div className="grid grid-cols-3 gap-4">
+                    <div>
+                        <div className="p-5 m-6 bg-blue-200 rounded-lg shadow">My medications</div>
+                            <div className="p-8">
+                                <div>Children need permission from their parents in the form of an authorization letter when they have to go on a school field trip.</div>
+                                <div>Children need permission from their parents in the form of an authorization letter when they have to go on a school field trip.</div>
+                                <div>Children need permission from their parents in the form of an authorization letter when they have to go on a school field trip.</div>
+                            </div>
+                    </div>
+                    
+                    <div>
+                        <div className="p-5 m-6 bg-blue-200 rounded-lg shadow">My appointments</div>
+                            <div className="p-8">]
+                                <div>Children need permission from their parents in the form of an authorization letter when they have to go on a school field trip.</div>
+                                <div>Children need permission from their parents in the form of an authorization letter when they have to go on a school field trip.</div>
+                                <div>Children need permission from their parents in the form of an authorization letter when they have to go on a school field trip.</div>
+                            </div>
+                    </div>
+                    
+                    <div>
+                        <div className="p-5 m-6 bg-blue-200 rounded-lg shadow">History</div>
+                            <div className="p-8">
+                                <div>Children need permission from their parents in the form of an authorization letter when they have to go on a school field trip.</div>
+                                <div>Children need permission from their parents in the form of an authorization letter when they have to go on a school field trip.</div>
+                                <div>Children need permission from their parents in the form of an authorization letter when they have to go on a school field trip.</div>
+                            </div>
+                    </div>        
+                        
+                
+                    
+                </div>
+            </div>
         </div>
     )
 }
