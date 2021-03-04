e3ae384317bb603e6ec508f1c55721addfbfbc5c
JanithKE <49825461+JanithKE@users.noreply.github.com>
2021-03-04 15:19:05 +0530

    edit

--- src/pages/Patient/Home/PatientHome.js
+++ src/pages/Patient/Home/PatientHome.js
@@ -2,13 +2,13 @@ const PatientHome = () => {
     return (
         <div>
             <div className="bg-gray-100 rounded-lg shadow">
-                <div className="p-9 font-mono">
+                <div className="p-9 mb-5 font-mono">
                     <h1 className="text-4xl">User name</h1>
                     <h3 className="text-xl">User id</h3>
                 </div>
-                <div>
-                    <button className="p-2 px-7 self-auto">
-                       <a href="#shedule page">Shedule</a>
+                <div className="p-2 px-10 rounded-lg  mx-3  w-36  bg-gray-300  shadow">
+                    <button >
+                       <a href="#shedule">Shedule</a>
                     </button>
                 </div>
             </div>
