9d9c8be5cc1995d1e4af85928340a3d86f5ca35e
psychonelli <60044237+psychonelli@users.noreply.github.com>
2021-03-05 00:44:30 +0530

    update diagnosis

--- src/pages/Doctor/Home/Components/Diagnosis.js
+++ src/pages/Doctor/Home/Components/Diagnosis.js
@@ -19,6 +19,7 @@ export const Diagnosis = () => {
       <div className="flex-1  text-center  px-4 py-2 m-2
              bg-blue-500  grid   gap-4 border 
              rounded-lg border-blue-400 hover:bg-blue-600">Prescripiton
+             <div className="mr-auto"><button className="bg-green-500  ">Add Medicine</button></div>
              
              <table class="border-separate ">
   <thead>
@@ -34,19 +35,33 @@ export const Diagnosis = () => {
       <td className="border w-96"><input className="w-96" type="text"></input></td>
         <td className="border"><input type="checkbox" ></input></td>
         <td className="border"><input type="checkbox"></input></td>
-        <td className="border"><input type="text"></input></td>
+        <td className="border w-56"><select>
+        <option>1</option>
+        <option>2</option>
+        </select></td>
     </tr>
     <tr>
-      <td class="border ">Ohio</td>
-      <td class="border ">Columbus</td>
+    <td className="border w-96"><input className="w-96" type="text"></input></td>
+        <td className="border"><input type="checkbox" ></input></td>
+        <td className="border"><input type="checkbox"></input></td>
+        <td className="border w-56"><select>
+        <option>1</option>
+        <option>2</option>
+        </select></td>
     </tr>
     <tr>
-      <td class="border">Michigan</td>
-      <td class="border">Detroit</td>
+    <td className="border w-96"><input className="w-96" type="text"></input></td>
+        <td className="border"><input type="checkbox" ></input></td>
+        <td className="border"><input type="checkbox"></input></td>
+        <td className="border w-56"><select>
+        <option>1</option>
+        <option>2</option>
+        </select></td>
     </tr>
   </tbody>
 </table>
 
+<div><button className="bg-green-600 hover:bg-green-700">Submit</button></div>
           
         </div>
 
