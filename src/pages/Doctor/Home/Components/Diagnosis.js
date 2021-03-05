375b58124db614975ddb6e73bfc80068b355ac51
psychonelli <60044237+psychonelli@users.noreply.github.com>
2021-03-05 13:34:28 +0530

    update daignosis

--- src/pages/Doctor/Home/Components/Diagnosis.js
+++ src/pages/Doctor/Home/Components/Diagnosis.js
@@ -5,13 +5,17 @@ export const Diagnosis = () => {
 
         <div className="flex-1  text-center  px-4 py-2 m-2
              bg-blue-500  grid grid-flow-col grid-rows-2 grid-cols-3 gap-4 border 
-             rounded-lg border-blue-400 hover:bg-blue-600">Syntopms
+             rounded-lg border-blue-400 hover:bg-blue-600">Syntopms<div>
+               <button className="bg-green-700 hover:bg-green-500">Add Syntopms</button></div>
              </div>
        
         
         <div className="flex-1  text-center  px-4 py-2 m-2
             bg-blue-500  grid grid-flow-col grid-rows-2 grid-cols-3 gap-4 border 
-             rounded-lg border-blue-400 hover:bg-blue-600">Referral</div>
+             rounded-lg border-blue-400 hover:bg-blue-600">Referral<div>
+               <input className="w-72 ml-24"></input>
+             </div>
+             </div>
 
 
 
@@ -19,23 +23,23 @@ export const Diagnosis = () => {
       <div className="flex-1  text-center  px-4 py-2 m-2
              bg-blue-500  grid   gap-4 border 
              rounded-lg border-blue-400 hover:bg-blue-600">Prescripiton
-             <div className="mr-auto"><button className="bg-green-500  ">Add Medicine</button></div>
+             <div className="mr-auto"><button className="bg-green-700 hover:bg-green-500 ">Add Medicine</button></div>
              
              <table class="border-separate ">
   <thead>
     <tr>
-      <th className="border ">Medicine</th>
+      <th className="border  ">Medicine</th>
       <th className="border ">Before meal</th>
-      <th className="border">After Meal</th>
+      <th className="border ">After Meal</th>
       <th className="border">Dosage</th>
     </tr>
   </thead>
   <tbody>
     <tr>
-      <td className="border w-96"><input className="w-96" type="text"></input></td>
-        <td className="border"><input type="checkbox" ></input></td>
-        <td className="border"><input type="checkbox"></input></td>
-        <td className="border w-56"><select>
+      <td className="border w-1/5"><input className="w-96" type="text"></input></td>
+        <td className="border w-20"><input type="checkbox" ></input></td>
+        <td className="border w-20"><input type="checkbox"></input></td>
+        <td className="border w-px"><select>
         <option>1</option>
         <option>2</option>
         </select></td>
@@ -61,7 +65,7 @@ export const Diagnosis = () => {
   </tbody>
 </table>
 
-<div><button className="bg-green-600 hover:bg-green-700">Submit</button></div>
+<div><button className="bg-green-700 hover:bg-green-500">Submit</button></div>
           
         </div>
 
