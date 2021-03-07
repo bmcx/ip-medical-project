4874810f33bebfa85cd3309fc044596ad814af4b
psychonelli <60044237+psychonelli@users.noreply.github.com>
2021-03-07 22:21:58 +0530

    calendar

--- src/pages/Patient/Home/PatientHome.js
+++ src/pages/Patient/Home/PatientHome.js
@@ -2,62 +2,19 @@ import { CalendarContainer } from "react-datepicker"
 import CalendarMonth from "./Components/CalenderMonth";
 import React from 'react';
 import { __RouterContext }  from "react-router";
+import 'react-calendar/dist/Calendar.css';
 
 
 export const PatientHome = () => {
     
     return (
-        <div>
-            <CalendarMonth>
-                <CalendarContainer></CalendarContainer>
+        <div className="ml-96">
+            <button className="bg-blue-500 hover:bg-blue-400 rounded-full p-2 m-2">+ Add Appointment</button>
+            <CalendarMonth className="w-6 place-itmes-end ml-96">
+            
+               
             </CalendarMonth>
-            <div className="bg-gray-100 rounded-lg shadow">
-                <div className="p-9 mb-5 font-mono">
-                    <h1 className="text-4xl">User name</h1>
-                    <h3 className="text-xl">User id</h3>
-                </div>
-                <div className="p-2 px-9 rounded  mx-3  w-36  bg-gray-300  shadow">
-                    <button >
-                       <a href="#shedule">Shedule</a>
-                    </button>
-                </div>
-            </div>
-            <div>
-                <div className="grid grid-cols-3 gap-4">
-                    <div>
-                        <div className="p-5 m-6 bg-blue-200 rounded-lg shadow">My medications</div>
-                            <div className="p-8">
-                                <ul className="list-disc">
-                                    <li>Children need permission from their parents in the form of an authorization letter when they have to go on a school field trip.</li>
-                                    <li>Children need permission from their parents in the form of an authorization letter when they have to go on a school field trip.</li>
-                                    <li>Children need permission from their parents in the form of an authorization letter when they have to go on a school field trip.</li>
-                                </ul>
-                            </div>
-                    </div>
-                    
-                    <div>
-                        <div className="p-5 m-6 bg-blue-200 rounded-lg shadow">My appointments</div>
-                            <div className="p-8">
-                                <ul className="list-disc">
-                                    <li>Children need permission from their parents in the form of an authorization letter when they have to go on a school field trip.</li>
-                                    <li>Children need permission from their parents in the form of an authorization letter when they have to go on a school field trip.</li>
-                                    <li>Children need permission from their parents in the form of an authorization letter when they have to go on a school field trip.</li>
-                                </ul>
-                            </div>
-                    </div>
-                    
-                    <div>
-                        <div className="p-5 m-6 bg-blue-200 rounded-lg shadow">History</div>
-                            <div className="p-8 ">
-                                <ul className="list-disc">
-                                    <li>Children need permission from their parents in the form of an authorization letter when they have to go on a school field trip.</li>
-                                    <li>Children need permission from their parents in the form of an authorization letter when they have to go on a school field trip.</li>
-                                    <li>Children need permission from their parents in the form of an authorization letter when they have to go on a school field trip.</li>
-                                </ul>
-                            </div>
-                    </div>        
-                </div>
-            </div>
+           
         </div>
     )
 }
