7d0ab9fec079aa50a115cff0c72427c478938090
psychonelli <60044237+psychonelli@users.noreply.github.com>
2021-03-09 21:46:43 +0530

    update patient home

--- src/pages/Patient/Home/Components/Appointments.js
+++ src/pages/Patient/Home/Components/Appointments.js
@@ -10,50 +10,92 @@ import 'react-calendar/dist/Calendar.css';
 export const Appointments = () => {
 
     return (
+        <div className="flex bg-gray-100 ">
 
-        <div className="ml-2 bg-color-gray ">
-            <div className="flex rounded-x1 p-2 ml-2">
-                <div className="m-5  ml-4 sm:flex ">
+            <div className="flex-1 px-4 py-2 
+             rounded-lg  ">
+
+                <div className="ml-4">
                     <a href="" className="flex item-center no-underline">
                         <div className="flex-shrink-0">
                             <img className="h-16 w-16 rounded-full border-2 border-white" src="logo192.png"></img>
                         </div>
                     </a>
 
+                    <div className="">User Name
+                    <div>User Id
+                
+                    </div>
+
+                </div>
+                </div>
+                <table class="border-separate  mt-4">
+          <thead>
+            <tr>
+              <th className="border  ">Medicine</th>
+              <th className="border ">Before meal</th>
+              <th className="border ">After Meal</th>
+              <th className="border">Dosage</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td className="border w-1/5"><input className="w-96" type="text"></input></td>
+              <td className="border w-20"><input className="ml-12" type="checkbox" ></input></td>
+              <td className="border w-20"><input className="ml-12" type="checkbox"></input></td>
+              <td className="border w-px"><select className="ml-12">
+                <option>1</option>
+                <option>2</option>
+              </select></td>
+            </tr>
+            <tr>
+              <td className="border w-96"><input className="w-96" type="text"></input></td>
+              <td className="border"><input className="ml-12" type="checkbox" ></input></td>
+              <td className="border"><input className="ml-12" type="checkbox"></input></td>
+              <td className="border w-24 ml-4"><select className="ml-12">
+                <option>1</option>
+                <option >2</option>
+              </select></td>
+            </tr>
+            <tr>
+              <td className="border w-96"><input className="w-96" type="text"></input></td>
+              <td className="border"><input className="ml-12" type="checkbox" ></input></td>
+              <td className="border"><input className="ml-12" type="checkbox"></input></td>
+              <td className="border "><select className="ml-12">
+                <option >1</option>
+                <option >2</option>
+              </select></td>
+            </tr>
+          </tbody>
+        </table>
+               
+
+
+            </div>
+
+
+
+<div >
+            <button className="bg-blue-500 hover:bg-blue-400 rounded-full p-2 mt-2  text-white ml-24 mb-2" >+Add Appointment</button>
+            <div className="flex flex-row justify-end   
+             
+             rounded-lg  hover:bg-blue-600">
+                 <div>
+                   <div>
+                       <CalendarMonth></CalendarMonth>
+                   </div>
                 </div>
 
 
+              </div>
+            
+                </div>
+
+
+
             </div>
-            <div className="ml-6">User Name
-                    <div>User Id
+        
 
-                    </div>
-            </div>
-            <div className="ml-14 mt-6">Current Prescription
-            <table>
-                <tr></tr>
-            </table>
-            </div>
-            <div className="flex flex-row justify-end mr-20">
-
-                <button className="bg-blue-500 hover:bg-blue-400 rounded-full p-2  text-white ">
-                    + Add Appointment</button>
-            </div>
-
-            <div className="flex flex-row justify-end  ">
-
-
-                <CalendarMonth >
-                    <CalendarContainer></CalendarContainer>
-
-
-                </CalendarMonth>
-            </div>
-
-
-
-
-        </div>
 
     )
 }
