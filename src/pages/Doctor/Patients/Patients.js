6d57472cb0ed7eb99823e2374cbd7f5626071756
Chandima Bandara <bmcbandara@hotmail.com>
2021-04-01 12:25:37 +0530

    add patitent appointment db connections

--- src/pages/Doctor/Patients/Patients.js
+++ src/pages/Doctor/Patients/Patients.js
@@ -1,163 +1,141 @@
-const Patients = () => {
-  return <div>Patients list
+import { firestoreConnect, isLoaded } from "react-redux-firebase";
 
+import { compose } from "redux";
+import { connect } from "react-redux";
+import { getFirestore } from "redux-firestore";
+import moment from "moment";
+import { useState } from "react";
 
-<div>
-      <table class="min-w-full table-auto">
-        <thead class="justify-between">
-          <tr class="bg-gray-800">
-            <th class="px-16 py-2">
-              <span class="text-gray-300"></span>
-            </th>
-            <th class="px-16 py-2">
-              <span class="text-gray-300">Name</span>
-            </th>
-            <th class="px-16 py-2">
-              <span class="text-gray-300">Appointments</span>
-            </th>
-            <th class="px-16 py-2">
-              <span class="text-gray-300">Date</span>
-            </th>
+const Patients = (props) => {
+  const { appointments, loaded } = props;
+  const [userPhoto, setUserPhoto] = useState("");
+  const [userName, setUserName] = useState("N/A");
 
-            <th class="px-16 py-2">
-              <span class="text-gray-300">Time</span>
-            </th>
+  const firestore = getFirestore();
 
-            <th class="px-16 py-2">
-              <span class="text-gray-300">Status</span>
-            </th>
-          </tr>
-        </thead>
-        <tbody class="bg-gray-200">
-          <tr class="bg-white border-4 border-gray-200">
-            <td class="px-16 py-2 flex flex-row items-center">
-              <img
-                class="h-8 w-8 rounded-full object-cover "
-               
-                alt=""
-              />
-            </td>
-            <td>
-              <span class="text-center ml-2 font-semibold">Deshan</span>
-            </td>
-            <td class="px-16 py-2">
-              <button class="bg-indigo-500 text-white px-4 py-2 border rounded-md hover:bg-white hover:border-indigo-500 hover:text-black ">
-                Open Link
-              </button>
-            </td>
-            <td class="px-16 py-2">
-              <span>05/06/2020</span>
-            </td>
-            <td class="px-16 py-2">
-              <span>10:00</span>
-            </td>
+  const onStatusChange = (status, id) => {
+    firestore
+      .collection("appointments")
+      .doc(id)
+      .update({
+        status: status ?? "PENDING",
+      });
+  };
 
-            <td class="px-16 py-2">
-              <span class="text-green-500">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  class="w-5 h5 "
-                  viewBox="0 0 24 24"
-                  stroke-width="1.5"
-                  stroke="#2c3e50"
-                  fill="none"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                >
-                  <path stroke="none" d="M0 0h24v24H0z" />
-                  <path d="M5 12l5 5l10 -10" />
-                </svg>
-              </span>
-            </td>
-          </tr>
-          <tr class="bg-white border-4 border-gray-200">
-            <td class="px-16 py-2 flex flex-row items-center">
-            <img
-                  class="h-8 w-8 rounded-full object-cover "
-                  
-                  alt=""
-                />
-            </td>
-            <td>
-              <span class="text-center ml-2 font-semibold">Adeepa</span>
-            </td>
-            <td class="px-16 py-2">
-              <button class="bg-indigo-500 text-white px-4 py-2 border rounded-md hover:bg-white hover:border-indigo-500 hover:text-black ">
-                Open Link
-              </button>
-            </td>
-            <td class="px-16 py-2">
-              <span>05/06/2020</span>
-            </td>
-            <td class="px-16 py-2">
-              <span>12:15</span>
-            </td>
+  const getPatientData = async (ref) => {
+    let patient = (await ref.get()).data();
+    setUserPhoto(patient.photo);
+    setUserName(`${patient.firstName} ${patient.lastName}`);
+  };
 
-            <td class="px-16 py-2">
-              <span class="text-yellow-500">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  class="w-5 h-5"
-                  viewBox="0 0 24 24"
-                  stroke-width="1.5"
-                  stroke="#2c3e50"
-                  fill="none"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                >
-                  <path stroke="none" d="M0 0h24v24H0z" />
-                  <circle cx="12" cy="12" r="9" />
-                  <polyline points="12 7 12 12 15 15" />
-                </svg>
-              </span>
-            </td>
-          </tr>
-          <tr class="bg-white border-4 border-gray-200">
-            <td class="px-16 py-2 flex flex-row items-center">
-            <img
-                  class="h-8 w-8 rounded-full object-cover "
-                  
-                  alt=""
-                />
-            </td>
-            <td>
-              <span class="text-center ml-2 font-semibold">Janith</span>
-            </td>
-            <td class="px-16 py-2">
-              <button class="bg-indigo-500 text-white px-4 py-2 border rounded-md hover:bg-white hover:border-indigo-500 hover:text-black ">
-                Open Link
-              </button>
-            </td>
-            <td class="px-16 py-2">
-              <span>05/06/2020</span>
-            </td>
-            <td class="px-16 py-2">
-              <span>08:35</span>
-            </td>
+  if (!loaded) return <div></div>;
+  return (
+    <div className="px-8 py-10 ">
+      <div className="text-xl mb-10 font-bold">Patient Appointments</div>
+      <div>
+        <table class="min-w-full table-auto">
+          <thead class="justify-between">
+            <tr class="bg-gray-800">
+              <th class="py-3 px-2 text-left w-16">
+                <span class="text-gray-300"></span>
+              </th>
+              <th class="py-3 px-2 text-left">
+                <span class="text-gray-300">Name</span>
+              </th>
+              <th class="py-3 px-2 text-left">
+                <span class="text-gray-300">Date</span>
+              </th>
 
-            <td class="px-16 py-2">
-              <span class="text-yellow-500">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  class="w-5 h-5"
-                  viewBox="0 0 24 24"
-                  stroke-width="1.5"
-                  stroke="#2c3e50"
-                  fill="none"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                >
-                  <path stroke="none" d="M0 0h24v24H0z" />
-                  <line x1="18" y1="6" x2="6" y2="18" />
-                  <line x1="6" y1="6" x2="18" y2="18" />
-                </svg>
-              </span>
-            </td>
-          </tr>
-        </tbody>
-      </table>
+              <th class="py-3 px-2 text-left">
+                <span class="text-gray-300">Time</span>
+              </th>
+
+              <th class="py-3 px-2 text-left w-16">
+                <span class="text-gray-300">Status</span>
+              </th>
+            </tr>
+          </thead>
+          <tbody class="bg-gray-200">
+            {appointments &&
+              appointments.map((appointment) => {
+                getPatientData(appointment.patient);
+                return (
+                  <tr
+                    key={appointment.id}
+                    class="border-b border-gray-200 hover:bg-gray-100"
+                  >
+                    <td class="py-3 px-2 text-left whitespace-nowrap">
+                      <img
+                        src={userPhoto}
+                        class="h-8 w-8 rounded-full object-cover "
+                        alt=""
+                      />
+                    </td>
+                    <td class="py-3 px-2 text-left whitespace-nowrap">
+                      <span class="font-semibold">{userName}</span>
+                    </td>
+
+                    <td class="py-3 px-2 text-left whitespace-nowrap">
+                      <span>
+                        {moment(appointment.dateTime?.toDate()).format(
+                          "DD MM YYYY"
+                        )}
+                      </span>
+                    </td>
+                    <td class="py-3 px-2 text-left whitespace-nowrap">
+                      <span>
+                        {moment(appointment.dateTime?.toDate()).format(
+                          "hh:mm A"
+                        )}
+                      </span>
+                    </td>
+
+                    <td class="py-3 px-2 text-left whitespace-nowrap">
+                      <select
+                        value={appointment.status}
+                        onChange={(e) =>
+                          onStatusChange(e.target.value, appointment.id)
+                        }
+                      >
+                        <option key="1" value="ACCEPTED">
+                          ACCEPTED
+                        </option>
+                        <option key="2" value="REJECTED">
+                          REJECTED
+                        </option>
+                        <option key="3" value="PENDING">
+                          PENDING
+                        </option>
+                      </select>
+                    </td>
+                  </tr>
+                );
+              })}
+          </tbody>
+        </table>
+      </div>
     </div>
-</div>
+  );
 };
 
-export default Patients;
+const mapStateToProps = (state, props) => {
+  return {
+    appointments: state.firestore.ordered.appointments ?? [],
+    loaded: isLoaded(state.firestore.ordered),
+    myId: state.firebase.auth.uid ?? "",
+  };
+};
+
+export default compose(
+  connect(mapStateToProps),
+  firestoreConnect((props) => {
+    console.log(props);
+    return [
+      {
+        collection: "appointments",
+        where: ["doctor", "==", props.myId],
+        orderBy: "dateTime",
+      },
+    ];
+  })
+)(Patients);
